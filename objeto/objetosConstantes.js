1eda03b5b68bec493b89c6ee5b8e5ba77803b602
JamAlves <jaymealves123@hotmail.com>
2022-06-19 15:29:51 -0300

    ObjConst.js

---
+++ objeto/objetosConstantes.js
@@ -0,0 +1,22 @@
+// pessoa -> 123 -> {...}
+
+const pessoa = {nome: 'Joao'}
+pessoa.nome = 'Pedro'
+
+console.log(pessoa)
+
+// pessoa -> 456 -> {...}
+//pessoa = {nome: 'Ana'}
+
+Object.freeze(pessoa)
+
+pessoa.nome = 'Maria'
+pessoa.end = 'Rua ABC'
+delete pessoa.nome
+
+console.log(pessoa)
+console.log(pesso.nome)
+
+const pessoaConstante = Object.freeze({nome: 'Joao'})
+pessoaConstante.nome = 'Maria'
+console.log(pessoaConstante)
