a987cd52bdde7f9067bf90777b8caa296fb0288a
JamAlves <jaymealves123@hotmail.com>
2022-09-18 21:22:38 -0300

    Promise_1.jsEx

--- funcao/promise_1.js
+++ funcao/promise_1.js
@@ -1,6 +1,34 @@
-let a = 1
-console.log(a)
+const { values } = require("lodash")
 
-//let p = new Promise()
+/*let p = new Promise(function(cumprirPromessa){
+    cumprirPromessa(['Jayme','Bia', 'Heloisa', 'Leo', 'Nayane'])
+})
 
-console.log(typeof Promise)
+function primeiroElemento(array){
+    return array[0]
+}
+
+function primeiraLetra(string){
+    return string[0]
+}
+
+const letraMinuscula = letra => letra.toLowerCase()
+
+p
+    .then(primeiroElemento)
+    .then(primeiraLetra)
+    .then(letraMinuscula)
+    .then(letraMinuscula => console.log(letraMinuscula))
+    */
+
+const primeiroElemento = arrayOuString => arrayOuString[0]
+const letraMinuscula = letra => letra.toLowerCase()
+
+new Promise(function (resolve) {
+    resolve(['Ana', 'Bia', 'Carlos', 'Daniel'])
+})
+    .then(primeiroElemento)
+    .then(primeiroElemento)
+    .then(letraMinuscula)
+    .then(console.log)
+
