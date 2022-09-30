69e5e2b0c78f359c89ff5abc5a9557671efb87bd
JamAlves <jaymealves123@hotmail.com>
2022-09-29 22:23:24 -0300

    promise3.jsEx

--- funcao/promise_3.js
+++ funcao/promise_3.js
@@ -4,8 +4,6 @@ function gerarNumerosEntre(min, max){
     if(min > max){
         [max, min] = [min, max]
     }
-
-
 return new Promise(resolve => {
     const fator = max - min + 1
     const aleatorio = parseInt(Math.random() * fator) + min
